3a24237863aa41ebdf1b7752e3607a1fa94e8319
B1nus <123@1>
2024-12-01 07:18:36 +0100

    Day 1 Part 2

--- 1.js
+++ 1.js
@@ -1,17 +1,20 @@
 const fs = require('node:fs');
 
+
+
 const data = fs.readFile('1.txt', 'utf8', (err, data) => {
   lines = data.split('\n');
   lines = lines.filter((line) => line.length > 0);
   left = lines.flatMap((x) => Number(x.split('   ')[0]));
   right = lines.flatMap((x) => Number(x.split('   ')[1]));
 
-  left.sort((a,b) => a - b);
-  right.sort((a,b) => a - b);
-  
   sum = 0;
-  left.forEach((l, li) => {
-    sum += Math.abs(l - right[li]);
+  left.forEach((l) => {
+    right.forEach((r) => {
+      if (l == r) {
+        sum += l;
+      }
+    })
   });
   console.log(sum);
 });
