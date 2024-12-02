e4ebf96bdcb274f0fcd180df1eec1a6b36061111
B1nus <123@1>
2024-12-02 08:11:58 +0100

    Day 2 Part 2

--- 2.js
+++ 2.js
@@ -4,20 +4,26 @@ const data = fs.readFileSync('2.txt', 'utf8');
 count = 0;
 lines = data.split('\n').filter((x) => x.length > 0);
 lines.forEach((line) => {
-  i = 2;
-  count += 1;
-  nums = line.split(' ').map(Number);
-  if (nums.length > 1) {
+  nums_ = line.split(' ').map(Number);
+  for (let j = 0; j < nums_.length; j ++) {
+    passed = true;
+    nums = nums_.slice(0);
+    nums.splice(j, 1);
+    i = 2;
     while (i < nums.length) {
       if (!((nums[i] > nums[i - 1] && nums[i - 1] > nums[i - 2]) || (nums[i] < nums[i - 1] && nums[i - 1] < nums[i - 2]))) {
-        count -= 1;
+        passed = false;
         break;
       }
-      if (Math.abs(nums[i] - nums[i - 1]) > 3 || Math.abs(nums[i - 1] - nums[i - 2]) > 3 || nums[i] == nums[i-1] || nums[i-1] == nums[i-2]) {
-        count -= 1;
+      if (Math.abs(nums[i] - nums[i - 1]) > 3 || nums[i] == nums[i-1] || Math.abs(nums[i - 1] - nums[i - 2]) > 3 || nums[i-1] == nums[i-2]) {
+        passed = false;
         break;
       }
       i += 1;
+    }
+    if (passed) {
+      count += 1;
+      break;
     }
   }
 });
