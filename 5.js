4f5031c6717e54d8ab6995361a7300b7515feb95
B1nus <123@1>
2024-12-05 09:57:52 +0100

    Day 5 Part 2

--- 5.js
+++ 5.js
@@ -5,18 +5,32 @@ const split = data.split("\n\n");
 const rules = split[0].split('\n').map(line => line.split('|').map(num => Number(num)));
 const updates = split[1].split("\n").filter((l) => l.length > 0).map((line) => line.split(',').map((num) => Number(num)));
 
-var count = 0;
-left = updates.filter(update => {
+function check_validity(update, rules) {
   for (let i = 0; i < rules.length; i++) {
     const rule = rules[i];
     if (update.includes(rule[0]) && update.includes(rule[1])) {
-      if (update.indexOf(rule[0]) > update.indexOf(rule[1])) {
-        return false;
+      const index1 = update.indexOf(rule[0]);
+      const index2 = update.indexOf(rule[1]);
+      if (index1 > index2) {
+        return [index1, index2];
       }
     }
   }
-  count += update[Math.floor(update.length / 2)]
-  return true;
+  return null;
+}
+
+var count = 0;
+left = updates.forEach(update => {
+  var validity = check_validity(update, rules);
+
+  if (validity != null) {
+    while (validity != null) {
+      [i1, i2] = validity;
+      [update[i1], update[i2]] = [update[i2], update[i1]];
+      validity = check_validity(update, rules);
+    }
+    count += update[Math.floor(update.length / 2)];
+  }
 });
 
 console.log(count);
