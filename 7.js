c4dea06435926123d053fc2cf3d5a7ecc2c7b9ef
B1nus <123@1>
2024-12-07 11:44:32 +0100

    Day 7 Part 2

--- 7.js
+++ 7.js
@@ -1,23 +1,33 @@
+// For the record: I cheated. I'm not proud of it.
 const fs = require("node:fs");
 const data = fs.readFileSync("7.txt", "utf8");
 const lines = data.split('\n').filter(l => l.length > 0);
 
 var sum = 0;
 lines.forEach(line => {
-  lhs = Number(line.slice(0,line.indexOf(':')));
-  rhs = line.slice(line.indexOf(':') + 2,line.length).split(' ').map(Number);
-  for (let i = 0; i < Math.pow(2, rhs.length - 1); i++) {
+  lhs = Number(line.slice(0, line.indexOf(':')));
+  rhs = line.slice(line.indexOf(':') + 2, line.length).split(' ').map(Number);
+  for (let i = 0; i < Math.pow(3, rhs.length - 1); i++) {
     result = rhs[0];
-    for (let n = 0; n < rhs.length- 1; n++) {
-      if ((i >> n) & 1 == 1) {
+    temp = i;
+    for (let n = 0; n < rhs.length - 1; n++) {
+      // This is the part I cheated to get.
+      op = temp % 3;
+      temp = Math.floor(temp / 3);
+      // End of cheat.
+      
+      if (op == 0) {
+        result = Number(result.toString().concat(rhs[n + 1].toString()));
+      } else if (op == 1) {
         result *= rhs[n + 1];
-      } else {
+      } else if (op == 2) {
         result += rhs[n + 1];
+
       }
     }
     if (result == lhs) {
       sum += result;
-      break;
+      return;
     }
   }
 });
