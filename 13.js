2d7121ee0f12f4d3847614263009a9511c24b0cd
1 <1>
2024-12-14 21:57:16 +0100

    Day 13 Part 2

--- 13.js
+++ 13.js
@@ -1,120 +1,35 @@
 const fs = require("node:fs");
 const data = fs.readFileSync("13.txt", "utf8");
+// For greater precision. Maybe I should've written it myself. I just wasn't up for doing that today.
+const Decimal = require("decimal.js");
 
 const paragraphs = data.split('\n\n');
 
-function gcd(a, b) {
-  if (b == 0) { return a }
-  return gcd(b, a % b);
-}
-
-// Find the numbers a and b such that ax + by = 1. Return x and y.
+// Quite devious of the puzzle creator to make it seem like there are more
+// than one solution to each price.
 //
-// I "borrowed" some code from https://www.w3resource.com/javascript-exercises/javascript-math-exercise-47.php
-function extended_euclid(a, b) {
-  a = +a;
-  b = +b;
-  if (a !== a || b !== b) {
-    return [NaN, NaN, NaN];
-  }
+// "The least expensive" actually just means "The only solution".
 
-  if (a === Infinity || a === -Infinity || b === Infinity || b === -Infinity) {
-    return [Infinity, Infinity, Infinity];
-  }
-  // Checks if a or b are decimals
-  if ((a % 1 !== 0) || (b % 1 !== 0)) {
-    return false;
-  }
-  var signX = (a < 0) ? -1 : 1,
-    signY = (b < 0) ? -1 : 1,
-    x = 0,
-    y = 1,
-    u = 1,
-    v = 0,
-    q, r, m, n;
-  a = Math.abs(a);
-  b = Math.abs(b);
-
-  while (a !== 0) {
-    q = Math.floor(b / a);
-    r = b % a;
-    m = x - u * q;
-    n = y - v * q;
-    b = a;
-    a = r;
-    x = u;
-    y = v;
-    u = m;
-    v = n;
-  }
-  return [b, signX * x, signY * y];
-}
-
-function get_diophantine(a, b, c) {
-  const d = gcd(a, b)
-  if (c % d == 0) {
-    a /= d;
-    b /= d;
-    c /= d;
-
-    let [_, x, y] = extended_euclid(a, b);
-
-    // Return one solution
-    return {
-      x0: x * c,
-      y0: y * c,
-      a: a,
-      b: b,
-    };
-  } else {
-    return null;
-  }
-}
-
-function diophantine_solution(diophantine, n) {
-  return [diophantine.x0 - diophantine.b * n, diophantine.y0 + diophantine.a * n];
-}
-
-// Find all solutions where both x and y are positive.
-function all_positive(diophantine) {
-  if (diophantine == null) { return new Set };
-  var n = Math.ceil(- diophantine.y0 / diophantine.a);
-  var solutions = new Set;
-  val = diophantine_solution(diophantine, n);
-  while (val[0] >= 0 && val[1] >= 0) {
-    solutions.add(val[0].toString().concat(',').concat(val[1].toString()));
-    n += 1;
-    val = diophantine_solution(diophantine, n);
-  }
-  return solutions;
-}
-
-var count = 0;
+var cost = Decimal(0);
 paragraphs.forEach(paragraph => {
   let lines = paragraph.split('\n');
-  let ax = Number(lines[0].slice(12, 14));
-  let ay = Number(lines[0].slice(18, 20));
-  let bx = Number(lines[1].slice(12, 14));
-  let by = Number(lines[1].slice(18, 20));
-  let price_str = lines[2].slice(9, lines[2].length).split(', ');
-  let x = Number(price_str[0]);
-  let y = Number(price_str[1].slice(2, price_str[1].length));
+  const [ax, ay] = lines[0].split('X+')[1].split(', Y+').map(Decimal);
+  const [bx, by] = lines[1].split('X+')[1].split(', Y+').map(Decimal);
+  const [x, y] = lines[2].split('X=')[1].split(', Y=').map(n => { return Decimal(n).plus(Decimal(10000000000000)); });
 
-  let x_solutions = all_positive(get_diophantine(ax, bx, x));
-  let y_solutions = all_positive(get_diophantine(ay, by, y));
-
-  let same = x_solutions.intersection(y_solutions);
-  x_solutions.forEach
-  
-
-  if (same.size > 0) {
-    var min = 0xFFFFFFFFFFFFFFFF;
-    same.forEach(solution => {
-      const [a, b] = solution.split(',').map(Number);
-      const cost = a * 3 + b;
-      if (cost < min) { min = cost; }
-    })
-    count += min;
+  // Make button A and B into lines in standard notation.
+  const [a1, b1, c1] = [ay, ax.negated(), 0];
+  const [a2, b2, c2] = [by, bx.negated(), by.mul(x).sub(bx.mul(y))];
+  // Find the intersections x position.
+  const x_upper = b1.mul(c2).sub(b2.mul(c1));
+  const x_lower = a2.mul(b1).sub(a1.mul(b2));
+  const x_ = x_upper.div(x_lower);
+  // Calculate the button presses.
+  const a = x_.div(ax);
+  const b = x.sub(x_).div(bx);
+  // Check if it is actually possible
+  if (a.mod(1) == 0 && b.mod(1) == 0) {
+    cost = cost.add(a.mul(3).add(b));
   }
 });
-console.log(count);
+console.log(cost);
