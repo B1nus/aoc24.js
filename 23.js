fbc099ea6edb238cffae8733e1c2181d333ac951
1 <1>
2024-12-23 09:46:13 +0100

    Day 23 Part 2

--- 23.js
+++ 23.js
@@ -1,6 +1,5 @@
 const fs = require("node:fs");
 const connections = fs.readFileSync("23.txt", "utf8").split('\n').filter(l => l.length > 0).map(l => l.split('-'));
-var groups = new Set;
 var map = {};
 
 connections.forEach(conn => {
@@ -17,16 +16,24 @@ connections.forEach(conn => {
   }
 });
 
-console.log(map);
-
-const part1 = Array.from(new Set(connections.flatMap(connection => {
-  const [a, b] = connection;
-  return Array
-    .from(map[a])
-    .filter(l => map[b].has(l))
-    .map(l => {
-      return connection.concat(l).sort().toString();
+var max = 0;
+var max_set = undefined;
+for (var computer in map) {
+  const same_array = Array.from(map[computer]);
+  for (let i = 0; i < Math.pow(2, map[computer].size); i++) {
+    var same = new Set(same_array);
+    same_array.forEach((other, index) => {
+      if (((i >> index) & 1) == 1) {
+        same = new Set([...map[other],...[other]].filter(l => same.has(l)));
+      } else {
+        same = new Set([...same].filter(l => l != other));
+      }
     });
-}))).map(group => group.split(',').map(l => l[0])).filter(group => group.indexOf('t') != -1).length;
+    if (same.size > max) {
+      max = same.size;
+      max_set = new Set([...same, ...[computer]]);
+    }
+  }
+}
+console.log([...max_set].sort().toString());
 
-console.log(part1);
